f0b61b64609d46ea65ee0407c350bf7012f39d02
Nar -- <28705606+finnar-bin@users.noreply.github.com>
2024-12-07 02:49:07 +0800

    feat: Custom Roles (#2481)
    # Description
    Added a page to manage an instance's custom roles.
    Closes #2479 
    
    ## Type of change
    - [x] New feature (non-breaking change which adds functionality)
    
    # Dependencies
    - [ ] https://github.com/zesty-io/fetch-wrapper/pull/66
    - [x] https://github.com/zesty-io/material/pull/109
    - [ ] https://github.com/zesty-io/accounts-api/pull/285
    - [ ] https://github.com/zesty-io/base-api/pull/207
    
    # How Has This Been Tested?
    - [x] Manual Test
    
    
    # Screenshots / Screen recording
    
    [screen-recorder-tue-oct-29-2024-15-02-58.webm](https://github.com/user-attachments/assets/e75765ef-8ab1-4936-b925-55f39977591a)
    
    ---------
    
    Co-authored-by: Stuart Runyan <stuart@zesty.io>

--- src/blocks/layoutsBlocks/revamp/layoutComponent/Images/index.js
+++ src/blocks/layoutsBlocks/revamp/layoutComponent/Images/index.js
@@ -1,7 +1,13 @@
-import ZestyImage from "blocks/Image/ZestyImage"
+import ZestyImage from 'blocks/Image/ZestyImage';
 
 function Images(props) {
-   
-    return <ZestyImage width={props?.data?.styles?.maxWidth}  style={...props?.data?.styles} alt={props?.data?.altText} src={props?.data?.content} />
+  return (
+    <ZestyImage
+      width={props?.data?.styles?.maxWidth}
+      style={{ ...props?.data?.styles }}
+      alt={props?.data?.altText}
+      src={props?.data?.content}
+    />
+  );
 }
-export default Images
+export default Images;
